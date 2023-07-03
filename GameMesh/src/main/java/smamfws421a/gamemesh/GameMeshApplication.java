cb0c996e3ef9dd90fa611e82f81da352d2ccab74
a-y-aksu <ali_yasin.aksu@edu.fhdw.de>
2023-07-03 16:23:02 +0200

    Grunderzeugung der Project Files

---
+++ GameMesh/src/main/java/smamfws421a/gamemesh/GameMeshApplication.java
@@ -0,0 +1,13 @@
+package smamfws421a.gamemesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameMeshApplication.class, args);
+	}
+
+}
