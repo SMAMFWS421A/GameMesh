94d5f3094660d72b7878bb383997db24830133c9
a-y-aksu <ali_yasin.aksu@edu.fhdw.de>
2023-08-29 14:38:21 +0200

    Vaadin Canvas

--- src/main/java/smamfws421a/gamemesh/components/ChessComponent.java
+++ src/main/java/smamfws421a/gamemesh/components/ChessComponent.java
@@ -1,2 +1,19 @@
-package smamfws421a.gamemesh.components;public class ChessComponent {
+package smamfws421a.gamemesh.components;
+
+import org.springframework.stereotype.Component;
+import org.vaadin.pekkam.Canvas;
+import org.vaadin.pekkam.CanvasRenderingContext2D;
+
+@Component
+public class ChessComponent {
+
+    public ChessComponent() {
+
+        Canvas canvas = new Canvas(1000, 1000);
+        CanvasRenderingContext2D ctx = canvas.getContext();
+
+
+
+
+    }
 }
--- src/main/java/smamfws421a/gamemesh/views/MainView.java
+++ src/main/java/smamfws421a/gamemesh/views/MainView.java
@@ -1,12 +1,13 @@
 package smamfws421a.gamemesh.views;
 
+import com.sun.jna.platform.unix.X11;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.html.Section;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
-import org.springframework.context.annotation.Bean;
+import smamfws421a.gamemesh.components.ChessComponent;
 import smamfws421a.gamemesh.components.NavbarMenu;
 import smamfws421a.gamemesh.security.SecurityService;
 
@@ -21,13 +22,16 @@ public class MainView extends AppLayout {
 
         this.securityService = securityService;
 
-
+        //navbar
         H1 title = new H1("GameMesh");
-
         NavbarMenu menu = new NavbarMenu(securityService);
-
         addToNavbar(title, menu.getMenu());
 
+
+        //chess type of bs
+
     }
+
+
 
 }
