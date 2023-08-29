7fa842024fb0ec2204a2d52d9b0b71651e980eb1
TimFroe <Tim_Jonathan.Froehlich@edu.fhdw.de>
2023-08-29 15:22:40 +0200

    recover files

---
+++ src/main/java/smamfws421a/gamemesh/chess/Figur.java
@@ -0,0 +1,41 @@
+package Chess.Game;
+
+import java.awt.image.BufferedImage;
+
+public abstract class Figur {
+
+    boolean color = false; //true = White
+    private FeldPosition boardpos;
+    private boolean firstmove;
+
+
+
+    public boolean getColor(){
+        return false;
+    }
+    public void setBoardpos(FeldPosition coordinate){
+        this.boardpos = coordinate;
+    }
+    public boolean[][] Bewegung(Feld board){
+        return initBewegArray(board);
+    }
+    public boolean[][] initBewegArray(Feld board){
+        int x = board.getFeldGröße()[0];
+        int y = board.getFeldGröße()[1];
+        boolean[][] BewegArray = new boolean[x][y];
+        return BewegArray;
+    }
+    public FeldPosition getCoordinate(){
+        return this.boardpos;
+    }
+
+    public BufferedImage getImage(){
+        return null;
+    }
+    public void setFirstMove(boolean firstMove){
+        this.firstmove = firstMove;
+    }
+    public boolean[][] Attack(Feld feld,int x, int y){
+        return Bewegung(feld);
+    }
+}
